ad204ba631ecb7216c902c3d9b915f64f1592925
ryuichi1988 <66043207+ryuichi1988@users.noreply.github.com>
2020-06-30 19:20:46 +0900

    datetime

---
+++ ttttttt.py
@@ -0,0 +1,9 @@
+from openpyxl.workbook import Workbook
+from datetime import datetime
+
+print(datetime.utcnow())
+a = 1
+
+a = +1
+a = +1
+print(a)
---
+++ ttttttttttttttttt6.py
@@ -0,0 +1,7 @@
+import kivy
+kivy.require('1.9.1')
+from kivy.app import App
+class TestApp(App):
+    pass
+if __name__ == '__main__':
+    TestApp().run()
