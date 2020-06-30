cd5fb9fd70c6c793312889654b551a0673abb36e
ryuichi1988 <66043207+ryuichi1988@users.noreply.github.com>
2020-06-30 20:27:57 +0900

    testapp

--- test4.py
+++ test4.py
@@ -1,24 +1,7 @@
-price = ''
-
-import requests
-import csv
-
-from bs4 import BeautifulSoup
-
-url = 'http://kakaku.com/kuruma/used/spec/Maker=1/Model=31773'
-html = requests.get(url)
-
-soup = BeautifulSoup(html.text,"html.parser")
-span = soup.find_all("span",{"class":"price"})
-
-for i in span:
-    print(i.string)
-    b =(i.string)
-
-a_list = []
-for j in b:
-    a_list.append(j)
-
-f = open('some.csv', 'w')
-writer = csv.writer(f)
-writer.writerow(a_list)
+words = "Hello word"
+print(words)
+print(repr(words))
+print(type(repr(words)))
+words = repr(words)
+print(words)
+print(repr(words))
